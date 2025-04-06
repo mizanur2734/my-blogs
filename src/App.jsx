ec2e36a3d07697305b16070306c46535fcb64141
mizanff <mrmizanurrahman626@gmail.com>
2025-04-05 21:36:26 -0500

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar></Navbar>
+
+      <div className="main-container flex text-center">
+        <div className="left-container w-[70%]">
+          <Blogs></Blogs>
+        </div>
+        <div className="right-container w-[30%]">
+          <h1>Reading time: 0</h1>
+          <h1>Bookmarked count: 0</h1>
+        </div>
+      </div>
     </>
   );
 }
